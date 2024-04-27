34503a750168c5a8ed7e69d5c219fa69926405f7
Emily Hudson <emily33901@gmail.com>
2024-04-28 00:50:36 +0100

    refactor process_output

--- media/src/color_conversion.rs
+++ media/src/color_conversion.rs
@@ -4,7 +4,10 @@ use eyre::{eyre, Result};
 use tokio::sync::mpsc;
 use windows::{
     core::Interface,
-    Win32::{Graphics::Direct3D11::ID3D11Texture2D, Media::MediaFoundation::*},
+    Win32::{
+        Graphics::Direct3D11::{ID3D11Device, ID3D11Texture2D},
+        Media::MediaFoundation::*,
+    },
 };
 
 use crate::{dx::ID3D11Texture2DExt, ARBITRARY_CHANNEL_LIMIT};
@@ -45,7 +48,7 @@ impl From<Format> for super::dx::TextureFormat {
     }
 }
 
-#[tracing::instrument]
+// #[tracing::instrument]
 pub(crate) async fn converter(
     output_width: u32,
     output_height: u32,
@@ -118,39 +121,37 @@ pub(crate) async fn converter(
                 attributes.set_u32(&MF_LOW_LATENCY, 1)?;
 
                 let set_format_types = |input_width, input_height| -> Result<()> {
+                    {
+                        let input_type = MFCreateMediaType()?;
 
+                        input_type.set_guid(&MF_MT_MAJOR_TYPE, &MFMediaType_Video)?;
+                        input_type.set_guid(&MF_MT_SUBTYPE, &input_format.into())?;
 
-                {
-                    let input_type = MFCreateMediaType()?;
+                        input_type.set_fraction(&MF_MT_FRAME_SIZE, input_width, input_height)?;
+                        input_type.set_u32(&MF_MT_ALL_SAMPLES_INDEPENDENT, 1)?;
 
-                    input_type.set_guid(&MF_MT_MAJOR_TYPE, &MFMediaType_Video)?;
-                    input_type.set_guid(&MF_MT_SUBTYPE, &input_format.into())?;
-
-                    input_type.set_fraction(&MF_MT_FRAME_SIZE, input_width, input_height)?;
-                    input_type.set_u32(&MF_MT_ALL_SAMPLES_INDEPENDENT, 1)?;
-
-                    transform.SetInputType(0, &input_type, 0)?;
-                }
-
-                for i in 0.. {
-                    let output_type = transform.GetOutputAvailableType(0, i)?;
-
-                    let format = output_type.get_guid(&MF_MT_SUBTYPE)?;
-
-                    output_type.set_fraction(&MF_MT_FRAME_SIZE, output_width, output_height)?;
-
-                    if format == output_format.into() {
-                        transform.SetOutputType(0, &output_type, 0)?;
-                        break;
+                        transform.SetInputType(0, &input_type, 0)?;
                     }
-                }
 
-                Ok(())
-            };
+                    for i in 0.. {
+                        let output_type = transform.GetOutputAvailableType(0, i)?;
 
-            set_format_types(output_width, output_height)?;
+                        let format = output_type.get_guid(&MF_MT_SUBTYPE)?;
 
-                let  (mut last_input_width ,mut last_input_height) = (output_width, output_height);
+                        output_type.set_fraction(&MF_MT_FRAME_SIZE, output_width, output_height)?;
+
+                        if format == output_format.into() {
+                            transform.SetOutputType(0, &output_type, 0)?;
+                            break;
+                        }
+                    }
+
+                    Ok(())
+                };
+
+                set_format_types(output_width, output_height)?;
+
+                let (mut last_input_width, mut last_input_height) = (output_width, output_height);
 
                 loop {
                     let ConvertControl::Frame(frame, time) = {
@@ -178,80 +179,51 @@ pub(crate) async fn converter(
                     // NOTE(emily): cc requires that the input texture be bind_render_target and bind_shader_resource
 
                     let sample = {
+                        let (width, height) = {
+                            let desc = frame.desc();
+                            (desc.Width, desc.Height)
+                        };
 
-                    let (width, height) = {
-                        let desc = frame.desc();
-                        (desc.Width, desc.Height)
+                        if width != last_input_width || height != last_input_height {
+                            tracing::debug!(
+                                width,
+                                height,
+                                last_input_width,
+                                last_input_height,
+                                "input changed"
+                            );
+                            // Change input type according to
+                            // https://learn.microsoft.com/en-us/windows/win32/medfound/handling-stream-changes
+                            // drain mft
+                            transform.ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, 0)?;
+                            let mut status = 0;
+                            while let Err(MF_E_TRANSFORM_NEED_MORE_INPUT) = transform
+                                .ProcessOutput(0, &mut [], &mut status)
+                                .map_err(|e| e.code())
+                            {}
+
+                            // Set the input type
+                            set_format_types(width, height)?;
+                            // Update our last types
+                            (last_input_width, last_input_height) = (width, height);
+                        }
+
+                        let texture = super::dx::TextureBuilder::new(
+                            &device,
+                            width,
+                            height,
+                            input_format.into(),
+                        )
+                        .bind_render_target()
+                        .bind_shader_resource()
+                        .build()?;
+
+                        super::dx::copy_texture(&texture, &frame, None)?;
+                        make_dxgi_sample(&texture, None)?
                     };
-
-                    if width != last_input_width || height != last_input_height {
-                        tracing::debug!(width, height, last_input_width, last_input_height, "input changed");
-                        // Change input type according to
-                        // https://learn.microsoft.com/en-us/windows/win32/medfound/handling-stream-changes
-                        // drain mft
-                        transform.ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, 0)?;
-                        let mut status = 0;
-                        while let Err(MF_E_TRANSFORM_NEED_MORE_INPUT) = transform.ProcessOutput(0, &mut [],  &mut status).map_err(|e| e.code()) {}
-
-                        // Set the input type
-                        set_format_types(width, height)?;
-                        // Update our last types
-                        (last_input_width, last_input_height) = (width, height);
-                    }
-
-                    let texture =
-                        super::dx::TextureBuilder::new(&device, width, height, input_format.into())
-                            .bind_render_target()
-                            .bind_shader_resource()
-                            .build()?;
-
-                    super::dx::copy_texture(&texture, &frame, None)?;
-                    make_dxgi_sample(&texture, None)?
-                };
 
                     sample.SetSampleTime(time.hns())?;
                     sample.SetSampleDuration(10_000_000 / target_framerate as i64)?;
-
-                    let process_output = || -> Result<Option<(ID3D11Texture2D, crate::Timestamp)>, windows::core::Error> {
-                        let mut output_buffer = MFT_OUTPUT_DATA_BUFFER::default();
-                        output_buffer.dwStatus = 0;
-                        output_buffer.dwStreamID = 0;
-
-                        let mut output_buffers = [output_buffer];
-
-                        let mut status = 0_u32;
-                        match transform.ProcessOutput(0, &mut output_buffers, &mut status) {
-                            Ok(_ok) => {
-                                let sample = output_buffers[0].pSample.take().unwrap();
-
-                                let timestamp_hns = unsafe { sample.GetSampleTime()? };
-
-                                let media_buffer = unsafe { sample.GetBufferByIndex(0) }?;
-                                let dxgi_buffer: IMFDXGIBuffer = media_buffer.cast()?;
-
-                                let (texture, subresource_index) = dxgi_buffer.texture()?;
-
-                                let output_texture = super::dx::TextureBuilder::new(
-                                    &device,
-                                    output_width,
-                                    output_height,
-                                    output_format.into(),
-                                )
-                                .keyed_mutex()
-                                .nt_handle()
-                                .build()
-                                .unwrap();
-
-                                copy_texture(&output_texture, &texture, Some(subresource_index))?;
-
-                                Ok(Some((output_texture, crate::Timestamp::new_hns(timestamp_hns))))
-                            }
-                            Err(err) => {
-                                Err(err)
-                            }
-                        }
-                    };
-
 
                     let result = transform
                         .ProcessInput(0, &sample, 0)
@@ -261,7 +233,14 @@ pub(crate) async fn converter(
 
                     match result {
                         Ok(_) | Err(MF_E_NOTACCEPTING) => {
-                            let output_result = process_output().map_err(|err| err.code());
+                            let output_result = process_output(
+                                &transform,
+                                &device,
+                                output_width,
+                                output_height,
+                                output_format,
+                            )
+                            .map_err(|err| err.code());
 
                             tracing::trace!("cc process output {output_result:?}");
 
@@ -316,4 +295,53 @@ pub(crate) async fn converter(
     });
 
     Ok((control_tx, event_rx))
+}
+
+fn process_output(
+    transform: &IMFTransform,
+    device: &ID3D11Device,
+    output_width: u32,
+    output_height: u32,
+    output_format: Format,
+) -> Result<Option<(ID3D11Texture2D, crate::Timestamp)>, windows::core::Error> {
+    unsafe {
+        let mut output_buffer = MFT_OUTPUT_DATA_BUFFER::default();
+        output_buffer.dwStatus = 0;
+        output_buffer.dwStreamID = 0;
+
+        let mut output_buffers = [output_buffer];
+
+        let mut status = 0_u32;
+        match transform.ProcessOutput(0, &mut output_buffers, &mut status) {
+            Ok(_ok) => {
+                let sample = output_buffers[0].pSample.take().unwrap();
+
+                let timestamp_hns = unsafe { sample.GetSampleTime()? };
+
+                let media_buffer = unsafe { sample.GetBufferByIndex(0) }?;
+                let dxgi_buffer: IMFDXGIBuffer = media_buffer.cast()?;
+
+                let (texture, subresource_index) = dxgi_buffer.texture()?;
+
+                let output_texture = super::dx::TextureBuilder::new(
+                    &device,
+                    output_width,
+                    output_height,
+                    output_format.into(),
+                )
+                .keyed_mutex()
+                .nt_handle()
+                .build()
+                .unwrap();
+
+                copy_texture(&output_texture, &texture, Some(subresource_index))?;
+
+                Ok(Some((
+                    output_texture,
+                    crate::Timestamp::new_hns(timestamp_hns),
+                )))
+            }
+            Err(err) => Err(err),
+        }
+    }
 }
